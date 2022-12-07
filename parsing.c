d9dda3842fb3ce02be354df86e83775308ee4025
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-12-07 18:45:23 +0100

    Made get_map_dimensions fetch the width and length of the map. It will also check for any invalid characters whilst doing so, reading the entire file once, then closing it and reopening it. Now going to write the function that stores the map in the data struct, using the information about how large the map is.

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: limartin <limartin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/02 17:16:10 by limartin      #+#    #+#                 */
-/*   Updated: 2022/12/02 19:59:37 by limartin      ########   odam.nl         */
+/*   Updated: 2022/12/07 18:39:26 by limartin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,38 +25,35 @@ void	save_valid_map(t_data *d, int fd)
 
 	i = 0;
 	d->map_len = 0;
-	gnl_ret = mini_gnl(fd);
-	while (gnl_ret != NULL)
-	{
-		d->map[i] = gnl_ret;
-		gnl_ret = mini_gnl(fd);
-	}
-	// while (gnl_ret != NULL)
-	// {
-	// 	d->map[i] = gnl_ret;
-	// 	gnl_ret = mini_gnl(fd);
-	// }
 	return ;
 }
 
-void get_map_dimensions(t_data *d, int *fd)
+void	get_map_dimensions(t_data *d, int *fd)
 {
-	int read_ret;
-	int	total_chars;
-	char buf;
+	int		read_ret;
+	char	buf;
 
 	d->map_width = 0;
 	d->map_len = 0;
-	while (d->map_len == 0)
+	read_ret = 1;
+	while (read_ret == 1)
 	{
-		read_ret = read(fd, &buf, 1);
+		read_ret = read(*fd, &buf, 1);
 		if (read_ret < 0)
 			ft_print_error("Error whilst reading from file.\n");
+		if (buf != '\n' && buf != '1' && buf != '0' && buf != 'C' && buf != 'P' \
+			&& buf != 'E')
+			ft_print_error("Map contains illegal characters\n");
 		if (read_ret == 0 || buf == '\n')
 			d->map_len++;
-		d->map_width++;
+		if (d->map_len == 0)
+			d->map_width++;
 	}
-	
+	close(*fd);
+	*fd = open(d->map_file, O_RDONLY);
+	if (fd < 0)
+		ft_print_error("Could not open map file.\n");
+	return ;
 }
 
 /* This function will exit() if an error is encountered, 
@@ -67,10 +64,11 @@ void	parsing(t_data *d, int argc, char **argv)
 
 	if (argc != 2)
 		ft_print_error("Please provide exactly one argument.\n");
-	if (ft_strlen(argv[1]) < 4 || \
-		ft_strcmp(argv[1] + ft_strlen(argv[1]) - 4, ".ber"))
+	d->map_file = argv[1];
+	if (ft_strlen(d->map_file) < 4 || \
+		ft_strcmp(d->map_file + ft_strlen(d->map_file) - 4, ".ber"))
 		ft_print_error("Map must be \".ber\" type.\n");
-	fd = open(argv[1], O_RDONLY);
+	fd = open(d->map_file, O_RDONLY);
 	if (fd < 0)
 		ft_print_error("Could not open map file.\n");
 	get_map_dimensions(d, &fd);
@@ -79,7 +77,7 @@ void	parsing(t_data *d, int argc, char **argv)
 }
 
 // save map as an array of strings (char **)
-// check for invalid characters
+
 // check if the map is rectangular (strs are equal length)
 // check if there is a requered amount of valid characters 
 //(1 exit, at least 1 collectable, 1 player)
